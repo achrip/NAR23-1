f8838478a6b59a2be882b8bc082360946d71aab6
achrip <ashraf.alif@gmail.com>
2022-10-31 19:01:48 +0700

    initial commit

---
+++ pre-training/java/javaIH1/src/Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    public int hpotion, apotion, cash; 
+
+    public Item(int hpotion, int apotion, int cash){
+        this.hpotion = hpotion;
+        this.apotion = apotion;
+        this.cash = cash; 
+    }
+}
