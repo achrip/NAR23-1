c118bba8191f4d61ba5ae8518279518122a62c2d
Achrip <ashraf.alif@gmail.com>
2022-11-06 19:07:15 +0700

    mended few missing parameters

--- pre-training/java/JavaProject_T148/src/Crypt.java
+++ pre-training/java/JavaProject_T148/src/Crypt.java
@@ -1,12 +1,10 @@
-import javax.crypto.Cipher;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
+import javax.crypto.Cipher; // class untuk enkripsi dan dekripsi data
+import javax.crypto.spec.SecretKeySpec; // class untuk bikin key dari password
+import java.util.Base64; // encoding dan decoding byte
 
 public class Crypt {
 
     protected final byte[] key = "Us3rS3CR3TD4T4SS".getBytes();
-    private String s;
     private Cipher cipher;
 
     private String encode(byte[] data){
@@ -18,10 +16,12 @@ public class Crypt {
     }
 
     public String encrypt(String item) throws Exception{
-        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        byte[] secretKey = new byte[16];
+        Base64.getDecoder().decode(key, secretKey);
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey, "AES");
 
         byte[] byteItem = item.getBytes();
-        cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         byte[] encryptedBytes = cipher.doFinal(byteItem);
         return encode(encryptedBytes);
@@ -29,11 +29,11 @@ public class Crypt {
     }
 
     public String decrypt(String cryptItem) throws Exception{
-        byte[] secretKey = new byte[16] ;
+        byte[] secretKey = new byte[16];
         Base64.getDecoder().decode(key, secretKey);
         SecretKeySpec keySpec = new SecretKeySpec(secretKey,"AES");
 
-        byte[] byteItem = Base64.getDecoder().decode(cryptItem);
+        byte[] byteItem = decode(cryptItem);
         cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         byte[] decryptedBytes = cipher.doFinal(byteItem);
