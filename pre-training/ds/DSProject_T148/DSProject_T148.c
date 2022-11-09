517035a8238a435dfc6753349cfbcc5ae2e0b8b4
Achrip <ashraf.alif@gmail.com>
2022-11-09 22:38:06 +0700

    password hiding work wonders

--- pre-training/ds/DSProject_T148/DSProject_T148.c
+++ pre-training/ds/DSProject_T148/DSProject_T148.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
 
 // generally used variables
 int choose;
 
 // TODO create a linked list for users, shows
 // TODO hash
+void lookup(); 
 void login(); 
 void signUp(); 
 void seeYa(); 
@@ -21,6 +24,7 @@ int main() {
     puts("3. exit"); 
     printf(">> "); 
     scanf("%d", &choose);
+    getchar(); 
 
     switch (choose)
     {
@@ -31,40 +35,67 @@ int main() {
         signUp(); 
         break; 
     case 3: 
-        seeYa(); 
+        // seeYa(); 
         break; 
     default:
         break;
     } 
 }
 
+void lookup() {
+    
+}
+
 void login() {
-    char user[255], password[255]; 
-    FILE *users = fopen("./users/users.txt", "r"); 
-
-    while (!feof(users)) { 
-        fscanf(users, "");
-    }
-
-    fclose(users); 
+    char user[255], password[255], c; 
+    int index = 0; 
 
     puts("Login");
     printf(">> name >> "); 
     scanf("%[^\n]", &user); 
     printf(">> password >> "); 
-    scanf("%s", &password); 
+    while ((c = getch()) != 13) {
+        if(index < 0)
+           index = 0;
+        if(c == 8){
+            putch('\b');
+            putch(NULL);
+            putch('\b');
+            index--;
 
-    if (/*user and password == user and password in linked list*/) home(); 
-    else return; 
+            continue;
+        }
+        password[index++] = c;
+        putch('*');
+   }
+   password[index] = '\0';
+    
+
+    if (/*user and password == user and password in linked list*/) { 
+        puts("login success"); 
+        printf("press any key to continue..."); 
+        getch(); 
+
+        home(); 
+    } else {
+        puts("login failed"); 
+        printf("press any key to continue..."); 
+        getch(); 
+
+        return; 
+    } 
 }
 
 void signUp() {
-    char user[255], password[255]; 
+    char user[255], password[255], c; 
+    int index = 0; 
+    bool check = false; 
     
     puts("Register"); 
     do {
         printf(">> name >> "); 
         scanf("%[^\n]", &user);
+        getchar(); 
 
         if (strlen(user) < 8) {
             puts("name must be at least 8 characters"); 
@@ -75,7 +106,41 @@ void signUp() {
         } else if (/* name exists */) {
             puts("name already taken!"); 
             continue; 
+        } else {
+            check = true; 
+        }
+    } while (check == false);
+    
+    do {
+        printf(">> password >> "); 
+        while ((c = getch()) != "\n") {
+            if(index < 0)
+                index = 0;
+            if(c == 8){
+                putch('\b');
+                putch(NULL);
+                putch('\b');
+                index--;
+
+                continue;
+            }
+            password[index++] = c;
+            putch('*');
+        }
+        password[index] = '\0'; 
+        
+        if (strlen(password) < 8) {
+            puts("password must be at least 8 characters"); 
+            continue; 
+        } else if (strlen(password) > 30) {
+            puts("password must not be more than 30 characters"); 
+            continue; 
+        } else {
+            check = true; 
         } 
-    } while (strlen(user) < 8 || strlen(user) > 30 || 
-        check == false)
+    } while (!check);
+
+    puts("Register Success"); 
+    printf("press any key to continue...");
+    getch();
 }
